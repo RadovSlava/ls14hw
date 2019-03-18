b4ae8f530d0f8c3a148c71106f53387621f5981a
RadovSlava <radov.slava@gmail.com>
2019-03-19 00:33:15 +0200

    1commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var x = prompt ("введи число");
+ var n = prompt ("введи степень"); 
+
+var Number = parseInt(x);
+var stepeni = parseInt(n);
+
+var result = Math.pow(x, n);
+
+alert("Result : "+result);
